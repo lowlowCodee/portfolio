1d3fc866f6554bdfbd99b92d581a705c1f62d4b7
lowlowCodee <laurenceg079@gmail.com>
2024-12-10 22:37:44 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+
+}
+
+
